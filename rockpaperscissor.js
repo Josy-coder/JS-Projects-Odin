afee42585697fd60c4990c1931807cb4c5de2bca
Baho Kemana Joselyto Charite <88556861+Josy-coder@users.noreply.github.com>
2023-06-26 13:15:52 +0200

    Update rockpaperscissor.js
    Added a function that select a random choice for computer and a part of a function that gets a tie with choices are same  and when computer choice is Paper while player choice is Rock

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -1,1 +1,19 @@
-console.log("Hello World")
+//function to get a random choice for computer
+
+function getComputerChoice () {
+  const choices = ["rock", "paper", "scissors"];
+  const randomOutput = Math.floor(Math.random() * choices.length);
+  return choices[randomOutput];
+}
+
+//a function to play a round of Rock Paper Scissor
+
+function roundOne (computerSelection, playerSelection) {
+let computerSelection = choices[randomOutput];
+let playerSelection = playerSelection.toLowerCase()
+
+if (playerSelection === computerSelection) {
+  return "Game tied";
+} else if (playerSelection === choices[0] && computerSelection === choices[1]) {
+return "Paper Beats Rock, Computer Wins";
+}
